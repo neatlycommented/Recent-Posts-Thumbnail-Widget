23d0ed0611a4714a1e185fa99d4de08b37170969
Zoe Rooney <hello@zoerooney.com>
2013-06-18 02:25:15 -0400

    Initial commit/ build

---
+++ README.md
@@ -0,0 +1,11 @@
+Recent Posts with Thumbnail
+===========================
+
+This is a simple WordPress plugin that creates a recent posts widget that displays the post thumbnail and, optionally, the post title and date. You can select the number of posts to display and choose to hide or show the title and date.
+
+Very little styling has been applied so that the theme designer, developer or use can easily customize the appearance.
+
+Changelog
+------------
+* 0.2 Added ability to show/hide titles and dates
+* 0.1 Initial build
---
+++ recent-posts-with-thumbnail-widget.php
@@ -0,0 +1,126 @@
+<?php
+/*
+Plugin Name: Recent Posts with Thumbnails Widget
+Plugin URI: http://zoerooney.com
+Description: Creates a widget that displays recent posts in a nice, easy to style layout.
+Version: 0.2
+Author: Zoe Rooney
+Author URI: http://zoerooney.com
+License: GPL2
+
+Copyright 2013 Zoe Roone (hello@zoerooney.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* Adds our Custom Social Media Icons widget
+=============================================*/
+
+class neatly_recent_posts_thumbnail extends WP_Widget {
+
+    function neatly_recent_posts_thumbnail() {
+        $widget_ops = array(
+            'classname'=>'neatly-recent', // class that will be added to li element in widgeted area ul
+            'description'=> __('Display recent posts with thumbnails') // description displayed in admin
+            );
+        $this->WP_Widget('neatly_recent_posts_thumbnail', __('Recent Posts with Thumbnails'), $widget_ops, $control_ops); // Name in  the control panel
+    }
+	
+	/* Our arguments
+	=============================================*/
+		
+    function widget($args, $instance) {
+            extract($args);
+			
+			$title = $instance['title']; 
+			$number = $instance['number'];
+			$show_title = isset($instance['show_title']) ? $instance['show_title'] : true;
+			$show_date = isset($instance['show_date']) ? $instance['show_date'] : true;
+		
+			/* Outputting our widget on the front end
+			=============================================*/
+			echo '<style>
+					.neatly-recent ul, .neatly-recent li { list-style:none; margin:0; }
+					.neatly-recent li { margin-bottom: 1em; }
+				  </style>';
+				  
+            echo $before_widget . $before_title . $title . $after_title; // widget title
+  			
+  			$args = array (
+  				'posts_per_page' => $number,
+  			);
+  			$neatly_posts = new WP_Query($args);
+  			if( $neatly_posts->have_posts() ) {
+  				echo '<ul>';
+  				while( $neatly_posts->have_posts() ) : $neatly_posts->the_post(); ?>
+  					<li><a href="<?php the_permalink(); ?>">
+  						<?php if ( has_post_thumbnail() ) : the_post_thumbnail(); endif; ?>
+  						<?php if ( $show_title == true ) : ?><h4><?php the_title(); ?></h4><?php endif; ?>
+  						<?php if ( $show_date == true ) : ?><span><?php the_date(); ?></span><?php endif; ?>
+  						</a>
+  					</li>
+  				<?php endwhile;
+  			}
+  			
+  			// Restore original Post Data
+  			wp_reset_postdata();
+            
+            echo $after_widget; // ends the widget
+        }
+	
+	/* Saving updated information
+	=============================================*/
+	
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        
+        $instance['title'] = strip_tags($new_instance['title']);
+        $instance['number'] = strip_tags($new_instance['number']);
+        $instance['show_title'] = $new_instance['show_title'] ? 1 : 0;
+        $instance['show_date'] = $new_instance['show_date'] ? 1 : 0; 
+          
+        return $instance;
+    }
+        
+	
+	/* The widget configuration form
+	=============================================*/
+	
+    function form( $instance ) {
+       $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) ); 
+        $title = strip_tags($instance['title']);
+        $number = strip_tags($instance['number']);
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+		<p><em>Use the following options to customize the display.</em></p>
+		
+		<p style="border-bottom:4px double #eee;padding: 0 0 10px;">
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>">Number of Posts Displayed</label>
+			<input id="<?php echo $this->get_field_id( 'number'); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo esc_attr($number); ?>" type="number" style="width:100%;" /><br>
+			<label for="<?php echo $this->get_field_id( 'show_title' ); ?>">Show the post titles?
+			<input id="<?php echo $this->get_field_id( 'show_title'); ?>" name="<?php echo $this->get_field_name( 'show_title' ); ?>" <?php checked($instance['show_title'], true) ?>  type="checkbox" /></label><br><br>
+			<label for="<?php echo $this->get_field_id( 'show_date' ); ?>">Show the post dates?
+			<input id="<?php echo $this->get_field_id( 'show_date'); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" <?php checked($instance['show_date'], true) ?>  type="checkbox" /></label>
+		</p>
+	    
+	    
+	 
+<?php
+	}
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("neatly_recent_posts_thumbnail");')); 
+
+?>
